563ac4f664ede8d9f0cfff8a8f4de94c2c5b0fac
futureniyi <futureniyi@yahoo.com>
2026-09-12 02:01:16 +0100

    W01 Assignment: .NET Applications

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindFiles(storesDirectory);
+
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+GenerateSalesSummary(salesFiles, storesDirectory, Path.Combine(salesTotalDir, "sales-summary.txt"));
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+void GenerateSalesSummary(IEnumerable<string> salesFiles, string storesDirectory, string reportPath)
+{
+    var details = new StringBuilder();
+    double totalSales = 0;
+
+    foreach (var file in salesFiles)
+    {
+        var fileTotal = CalculateSalesTotal(new[] { file });
+        totalSales += fileTotal;
+
+        // Include the store folder to distinguish files with the same name.
+        var fileName = Path.GetRelativePath(storesDirectory, file);
+        details.AppendLine($" {fileName}: {fileTotal:C}");
+    }
+
+    var report = new StringBuilder();
+    report.AppendLine("Sales Summary");
+    report.AppendLine("----------------------------");
+    report.AppendLine($" Total Sales: {totalSales:C}");
+    report.AppendLine();
+    report.AppendLine(" Details:");
+    report.Append(details);
+
+    File.WriteAllText(reportPath, report.ToString());
+}
+
+record SalesData(double Total);
